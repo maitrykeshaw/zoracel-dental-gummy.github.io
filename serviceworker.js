433de9761ee7e2b428cb679647e78abbdcab62f8
keshawmaitry <maitrykeshaw@gmail.com>
2025-04-27 10:53:44 +0530

    asda

---
+++ serviceworker.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
